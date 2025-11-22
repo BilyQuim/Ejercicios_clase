1b8b6108bfc0c61343f0707c34c902e683d3ebc3
billy <billy@gmail.com>
2025-11-21 22:38:43 -0600

    Proyecto 4

---
+++ TiendaFacturacion/AppFacturacion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BibliotecaDescuentos;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== SISTEMA DE FACTURACIÓN ===");
+        Console.Write("Ingrese el total de la compra: Q. ");
+        decimal total = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("\nTipo de cliente:");
+        Console.WriteLine("1. Regular");
+        Console.WriteLine("2. VIP");
+        Console.WriteLine("3. Empleado");
+        Console.Write("Seleccione opción: ");
+        int tipo = Convert.ToInt32(Console.ReadLine());
+
+        IDescuento descuento;
+
+        switch (tipo)
+        {
+            case 1: descuento = new DescuentoRegular(); break;
+            case 2: descuento = new DescuentoVIP(); break;
+            case 3: descuento = new DescuentoEmpleado(); break;
+            default:
+                Console.WriteLine("Opción no válida.");
+                return;
+        }
+
+        decimal totalFinal = descuento.AplicarDescuento(total);
+        Console.WriteLine($"\nTotal a pagar con descuento: Q. {totalFinal}");
+    }
+}
---
+++ TiendaFacturacion/BibliotecaDescuentos/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace BibliotecaDescuentos
+{
+    public interface IDescuento
+    {
+        decimal AplicarDescuento(decimal totalCompra);
+    }
+
+    public class DescuentoRegular : IDescuento
+    {
+        public decimal AplicarDescuento(decimal totalCompra)
+        {
+            return totalCompra;
+        }
+    }
+
+    public class DescuentoVIP : IDescuento
+    {
+        public decimal AplicarDescuento(decimal totalCompra)
+        {
+            return totalCompra * 0.90m;
+        }
+    }
+
+    public class DescuentoEmpleado : IDescuento
+    {
+        public decimal AplicarDescuento(decimal totalCompra)
+        {
+            return totalCompra * 0.80m;
+        }
+    }
+}
